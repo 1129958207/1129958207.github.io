e4333bef2284d87dbf01f4052b8ae9b58bb609b5
linbin <linbin1@xiaoyouzi.com>
2020-04-23 11:28:35 +0800

    feature:提交新的点击标记（友盟）

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ console.log('1::', +new Date());
 document.querySelector('.div12').onclick = function() {
   _czc.push(['_trackEvent', '测试点击统计', '点击', 'div12']);
 };
+
+document.querySelector('.div13').onclick = function() {
+  _czc.push(['_trackEvent', '测试点击统计', '点击', 'div13']);
+};
